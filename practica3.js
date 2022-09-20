15d9b6489af21b8636465a530ec3668a98ae8e93
manuelsernaeugenio <mansereug@alu.edu.gva.es>
2022-09-20 20:04:54 +0200

    Practica 3 subida

---
+++ practica3.js
@@ -0,0 +1,42 @@
+[
+    {
+        "url": "http://www.ejemplo.com/index.html",
+        "path": "/home/daw/web/index.html"
+    },
+    {
+        "url": "http://www.ejemplo.com/index.html",
+        "path": "/home/daw/web/index.html"
+    },
+    {
+        "url": "http://www.ejemplo.com/paginas/index.html",
+        "path": "/home/daw/web/paginas/index.html"
+    },
+    {
+        "url": "http://www.ejemplo.com/web/css/base.css",
+        "path": "/home/daw/web/web/css/base.css"
+    },
+    {
+        "url": "http://www.ejemplo.com/web/img/logo/grande.jpg",
+        "path": "/home/daw/web/web/img/logo/grande.jpg"
+    },
+    {
+        "url": "http://www.ejemplo.com/web/img/logo/editado/peque.jpeg",
+        "path": "/home/daw/web/web/img/logo/editado/peque.jpeg"
+    },
+    {
+        "url": "http://www.ejemplo.com/a/b/c/d.html",
+        "path": "/home/daw/web/a/b/c/d.html"
+    },
+    {
+        "url": "http://www.ejemplo.com/a/b/c/d.html",
+        "path": "/home/daw/web/a/b/c/d.html"
+    },
+    {
+        "url": "http://www.ejemplo.com/grupos.txt",
+        "path": "/home/daw/web/grupos.txt"
+    },
+    {
+        "url": "http://www.ejemplo.com/daw/videos/presentacion.webm",
+        "path": "/home/daw/web/daw/videos/presentacion.webm"
+    }
+]
